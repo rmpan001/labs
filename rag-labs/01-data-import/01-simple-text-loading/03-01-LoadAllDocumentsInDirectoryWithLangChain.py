39dc742e7a179fe6966c108a5dcaa07c38b84b0f
rmpan001 <rmpan001@odu.edu>
2026-09-15 20:28:16 -0400

    updated script to load sample text file docs in sample_docs directory

--- rag-labs/01-data-import/01-simple-text-loading/03-01-LoadAllDocumentsInDirectoryWithLangChain.py
+++ rag-labs/01-data-import/01-simple-text-loading/03-01-LoadAllDocumentsInDirectoryWithLangChain.py
@@ -1,7 +1,8 @@
 import os
 from langchain_community.document_loaders import DirectoryLoader
+from langchain_community.document_loaders import TextLoader
 
 ##Load all documents from a directory
-loader = DirectoryLoader("C:/Users/mpanu/Downloads/")
+loader = DirectoryLoader("./sample_docs/", glob="*.txt", loader_cls=TextLoader)
 documents = loader.load()
 print(f'number of documents loaded: {len(documents)}')
